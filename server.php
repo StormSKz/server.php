104be2e84764343ee206585e1c84d112ed3ad96f
StormSKz <81472967+StormSKz@users.noreply.github.com>
2021-04-15 11:30:19 +0700

    Update server.php

--- server.php
+++ server.php
@@ -1,30 +1,87 @@
-local http_request = http_request;
-if syn then
-	http_request = syn.request
-elseif SENTINEL_V2 then
-	function http_request(tb)
-		return {
-			StatusCode = 200;
-			Body = request(tb.Url, tb.Method, (tb.Body or ''))
+<?php
+/*  i am not a very great php coder. sorry for the messy code :) */
+/* list of headers just incase another exploit shows up and adds it */
+ 
+$hwidTypes = array("Syn-Fingerprint", "Exploit-Guid", "Proto-User-Identifier", "Sentinel-Fingerprint");
+/* the reason i made a key for each exploit is to demonstrate the differences between the HWID for each exploit that supports a header */
+/* i suggest that you clear this database or i will be able to use your script lol */
+ 
+$database = array(
+	array(
+		"key" => "key_sentinel",
+		"hwid" => "",
+	),
+	array(
+		"key" => "key_sirhurt",
+		"hwid" => "81ECAE2DD1AAADA537121E61D5C0280C"
+	),
+	array(
+		"key" => "key_protosmasher",
+		"hwid" => ""
+	),
+	array(
+		"key" => "key_synapse",
+		"hwid" => ""
+	),
+);
+ 
+function fetch_hwid($headers) {
+	global $hwidTypes;
+ 
+	$hwid = NULL;
+	$count = 0;
+ 
+	foreach($hwidTypes as $key => $header) {
+		if (isset($headers[$header])) {
+			$count = $count + 1;
+			$hwid = $headers[$header];
+		};
+	}
+ 
+	if ($count > 1) {
+		/* prevent people from setting multiple of the headers to bypass checks :) */
+		/* does this work?? */
+ 
+		$hwid = NULL;
+	}
+ 
+	return $hwid;	
+}
+ 
+if (isset($_GET["key"])) {
+	$hwid = fetch_hwid(getallheaders());
+	if (!is_null($hwid)) {
+		$didUserPass = FALSE;
+		$errorMsg = "authentication failed";
+		$key = $_GET["key"];
+ 
+		foreach($database as $_ => $user) {
+			$stored_key = $user["key"];
+			$stored_hwid = $user["hwid"];
+			$key_matches = ($stored_key == $key);
+			$hwid_matches = ($stored_hwid == $hwid);
+ 
+			if ($key_matches and $hwid_matches) {
+				$didUserPass = TRUE;
+ 
+				/* hey, they are whitelisted! give them the correct response */
+ 
+				echo(hash("sha256", $stored_key));
+			} elseif ($key_matches and !$hwid_matches) {
+				/* hey, why does their hardware id not match but key does? NO SHARING ALLOWED!*/
+ 
+				$errorMsg = "hwid mismatch";
+			}
 		}
-	end
-end
  
-local body = http_request({Url = 'https://httpbin.org/get'; Method = 'GET'}).Body;
-local decoded = game:GetService('HttpService'):JSONDecode(body)
-local hwid_list = {"Syn-Fingerprint", "Exploit-Guid", "Proto-User-Identifier", "Sentinel-Fingerprint"};
-local hwid = "";
+		if (!$didUserPass) {
+			echo($errorMsg);
+		};
+	} else {
+		die("no hwid supplied");
+	}
+} else {
+	die("invalid key");
+}
  
-for i, v in next, hwid_list do
-	if decoded.headers[v] then
-		hwid = decoded.headers[v];
-		break
-	end
-end
- 
-if hwid then
-	setclipboard(hwid)
-	game:GetService("Players").LocalPlayer:Kick('copied hwid to clipboard')
-else
-	game:GetService("Players").LocalPlayer:Kick('unable to find hwid')
-end
+?>
